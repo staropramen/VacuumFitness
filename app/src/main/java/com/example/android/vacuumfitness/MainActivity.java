672ea34ab14a48c31be9690373d41f8578909869
Daniel Spiess <dspiess82@gmail.com>
2019-05-26 09:07:32 -0300

    Add Butterknife and  Collapsing Toolbar

--- app/src/main/java/com/example/android/vacuumfitness/MainActivity.java
+++ app/src/main/java/com/example/android/vacuumfitness/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.android.vacuumfitness;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar) Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Setup Butterknife
+        ButterKnife.bind(this);
+
+        //Setup Toolbar
+        toolbar.setTitle(R.string.app_name);
     }
 }
