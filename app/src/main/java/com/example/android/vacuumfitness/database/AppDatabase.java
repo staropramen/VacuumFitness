0586b6fb6f94c0a3985e024de4244c78411379f0
Daniel Spiess <dspiess82@gmail.com>
2019-05-26 17:25:15 -0300

    Database Created

--- app/src/main/java/com/example/android/vacuumfitness/database/AppDatabase.java
+++ app/src/main/java/com/example/android/vacuumfitness/database/AppDatabase.java
@@ -1,4 +1,57 @@
 package com.example.android.vacuumfitness.database;
 
-public class AppDatabase {
+import android.arch.persistence.db.SupportSQLiteDatabase;
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.example.android.vacuumfitness.model.Exercise;
+
+import java.util.concurrent.Executors;
+
+@Database(entities = {Exercise.class}, version = 1, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static final String LOG_TAG = AppDatabase.class.getSimpleName();
+    private static final Object LOCK = new Object();
+    private static final String DATABASE_NAME = "vacuum-fitness";
+    private static AppDatabase sInstance;
+
+    public static AppDatabase getInstance(final Context context) {
+        if (sInstance == null) {
+            synchronized (LOCK) {
+                Log.d(LOG_TAG, "Creating new database instance");
+                sInstance = Room.databaseBuilder(context.getApplicationContext(),
+                        AppDatabase.class, AppDatabase.DATABASE_NAME)
+                        .addCallback(new Callback() {
+                            @Override
+                            public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                                super.onCreate(db);
+                                Executors.newSingleThreadScheduledExecutor().execute(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        getInstance(context).exerciseDao().insertAll(populateExercises());
+                                    }
+                                });
+                            }
+                        })
+                        .build();
+            }
+        }
+        Log.d(LOG_TAG, "Getting the database instance");
+        return sInstance;
+    }
+
+    public abstract ExerciseDao exerciseDao();
+
+    private static Exercise[] populateExercises(){
+        return new Exercise[] {
+                new Exercise("Silent Tree", 1, "dummy-picture", "xMQwBMhaqFA"),
+                new Exercise("Holly Wood", 1, "dummy-picture", "xMQwBMhaqFA"),
+                new Exercise("Rainy Day", 1, "dummy-picture", "xMQwBMhaqFA")
+        };
+    }
 }
---
+++ app/src/main/java/com/example/android/vacuumfitness/database/ExerciseDao.java
@@ -0,0 +1,34 @@
+package com.example.android.vacuumfitness.database;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import com.example.android.vacuumfitness.model.Exercise;
+
+import java.util.List;
+
+@Dao
+public interface ExerciseDao {
+    @Query("SELECT * FROM exercises ORDER BY primaryKey")
+    LiveData<List<Exercise>> loadAllExercises();
+
+    @Insert
+    void insertExercise(Exercise exercise);
+
+    @Insert
+    void insertAll(Exercise... exercises);
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    void updateExercise(Exercise exercise);
+
+    @Delete
+    void deleteExercise(Exercise exercise);
+
+    @Query("SELECT * FROM exercises WHERE primaryKey = :id")
+    LiveData<Exercise> loadExerciseById(int id);
+}
