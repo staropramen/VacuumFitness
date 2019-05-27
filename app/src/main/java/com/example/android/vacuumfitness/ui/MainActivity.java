c7b654076df66982ed0d692bb725bde60c4fd2d2
Daniel Spiess <dspiess82@gmail.com>
2019-05-27 17:04:01 -0300

    Design Training Layout

---
+++ app/src/main/java/com/example/android/vacuumfitness/ui/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.android.vacuumfitness.ui;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.example.android.vacuumfitness.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar) Toolbar toolbar;
+    @BindView(R.id.start_training_button) ImageView startTraining;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Setup Butterknife
+        ButterKnife.bind(this);
+
+        //Setup Toolbar
+        toolbar.setTitle(R.string.app_name);
+        setSupportActionBar(toolbar);
+
+        //Set onClick for StartTraining
+        startTraining.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation animFadein = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
+
+                startTraining.startAnimation(animFadein);
+                Intent intent = new Intent(MainActivity.this, TrainingActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
