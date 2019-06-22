447294edbda0cc91da11cb1a767c45fa2e517e3b
Daniel Spiess <dspiess82@gmail.com>
2019-06-22 12:32:36 -0300

    Custom Training Activity created

---
+++ app/src/main/java/com/example/android/vacuumfitness/ui/CustomTrainingActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.vacuumfitness.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CustomTrainingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_training);
+    }
+}
