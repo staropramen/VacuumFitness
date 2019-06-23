fd60aaed7c94be7575f0f4e6012a96730e477929
Daniel Spiess <dspiess82@gmail.com>
2019-06-23 10:34:50 -0300

    Customize Training Fragment created

--- app/src/main/java/com/example/android/vacuumfitness/ui/CustomTrainingActivity.java
+++ app/src/main/java/com/example/android/vacuumfitness/ui/CustomTrainingActivity.java
@@ -2,6 +2,7 @@ package com.example.android.vacuumfitness.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.example.android.vacuumfitness.R;
 
 public class CustomTrainingActivity extends AppCompatActivity {
 
