ff0a488d2a9d4063e60aaad56c8d5af2188e4bb5
Daniel Spiess <dspiess82@gmail.com>
2019-06-23 14:29:21 -0300

    Created CustomTrainingAdapter

---
+++ app/src/main/java/com/example/android/vacuumfitness/adapter/CustomTrainingAdapter.java
@@ -0,0 +1,83 @@
+package com.example.android.vacuumfitness.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.android.vacuumfitness.R;
+import com.example.android.vacuumfitness.model.Training;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class CustomTrainingAdapter extends RecyclerView.Adapter<CustomTrainingAdapter.CustomTrainingAdapterViewHolder> {
+
+    private List<Training> trainings;
+
+    private final CustomTrainingOnClickHandler customTrainingOnClickHandler;
+
+    public interface CustomTrainingOnClickHandler {
+        void onClick(Training training);
+    }
+
+    //Constructor
+    public CustomTrainingAdapter(CustomTrainingOnClickHandler clickHandler){customTrainingOnClickHandler = clickHandler;}
+
+    //ViewHolder
+    public class CustomTrainingAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        @BindView(R.id.tv_label) TextView labelTextView;
+        @BindView(R.id.tv_custom_training_name) TextView nameTextView;
+
+        public CustomTrainingAdapterViewHolder(View view){
+            super(view);
+            ButterKnife.bind(this, view);
+            view.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int adapterPosition = getAdapterPosition();
+            Training training = trainings.get(adapterPosition);
+            customTrainingOnClickHandler.onClick(training);
+        }
+    }
+
+    @NonNull
+    @Override
+    public CustomTrainingAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        Context context = viewGroup.getContext();
+        int listItem = R.layout.custom_training_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        boolean shouldAttachToParentImmediately = false;
+
+        View view = inflater.inflate(listItem, viewGroup,shouldAttachToParentImmediately);
+
+        return new CustomTrainingAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomTrainingAdapterViewHolder viewHolder, int position) {
+        Training training = trainings.get(position);
+        viewHolder.labelTextView.setText(training.getLabel());
+        viewHolder.nameTextView.setText(training.getTrainingName());
+    }
+
+    @Override
+    public int getItemCount() {
+        if (null == trainings) return 0;
+        return trainings.size();
+    }
+
+    //Function to set moviesArray
+    public void setTrainings(List<Training> trainingsToSet){
+        trainings = trainingsToSet;
+        notifyDataSetChanged();
+    }
+}
