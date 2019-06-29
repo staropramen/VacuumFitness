086671361bdf2ddd79b55578faa807af6585a8c6
Daniel Spiess <dspiess82@gmail.com>
2019-06-29 10:21:20 -0300

    Fragments for CustomizeMusicActivity initialized

---
+++ app/src/main/java/com/example/android/vacuumfitness/ui/CustomizeMusicActivity.java
@@ -0,0 +1,15 @@
+package com.example.android.vacuumfitness.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.android.vacuumfitness.R;
+
+public class CustomizeMusicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customize_music);
+    }
+}
