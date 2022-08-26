99ceb487914272562ecc882e7a486e9066bca396
Daniel Spiess <dspiess82@gmail.com>
2022-08-26 15:52:37 +0200

    About Fragment and subfragments working

---
+++ app/src/main/java/com/example/android/vacuumfitness/ui/TutorialFragment.java
@@ -0,0 +1,22 @@
+package com.dsconsult.android.vacuumfitness.ui;
+
+import android.os.Bundle;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import com.dsconsult.android.vacuumfitness.R;
+
+public class TutorialFragment extends Fragment {
+    @Override // android.support.v4.app.Fragment
+    public View onCreateView(LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle bundle) {
+        View inflate = layoutInflater.inflate(R.layout.fragment_tutorial, viewGroup, false);
+        ((Toolbar) getActivity().findViewById(R.id.toolbar))
+                .setTitle(getString(R.string.how_to_exercise));
+        return inflate;
+    }
+}
